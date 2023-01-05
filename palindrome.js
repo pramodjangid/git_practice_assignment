8b917f7fc9ada50059cb32c22340e7980e0806ee
pramodjangid <jangidpramod99@gmail.com>
2023-01-05 14:03:00 +0530

    I wrote the code

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+let str=`naman`
+
+let bag="";
+    for(i=str.length;i>=0;i++){
+        bag=bag+str[i]
+    }
+    if (bag=str){
+        console.log("Yes")
+    }
+    else{
+        console.log("No")
+    }
---
+++ prime.js
@@ -0,0 +1,14 @@
+let num=17;
+
+let count=1;
+    for(let i=0;i<=num;i+){
+        if (num%i==0){
+            count++;
+        }
+    }
+    if (count==1){
+        console.log("Yes")
+    }
+    else{
+        console.log("No")
+    }
